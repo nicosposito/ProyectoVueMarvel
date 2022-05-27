dc4e80def0ec415a5f63a61604d3b0746c21c6fb
nicosposito <nicolas.sposito16@hotmail.com>
2022-05-26 21:58:12 -0300

    Casi terminado pantalla de inicio

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,17 +1,18 @@
 <template>
   <!--Fondo-->
-
-
+  <span class="bg"></span>
   <!--Inicio-->
   <div>
     <b-img
-        :src=urlLogo
-        fluid
-        v-bind="imgOpcProps"
-        class="centerImg"
-      ></b-img>
+      :src="urlLogo"
+      fluid
+      v-bind="imgOpcProps"
+      class="centerImg mostrar"
+    ></b-img>
   </div>
-  <b-row align-h="center">
+
+  <!--Opciones-->
+  <b-row align-h="center " class="mostrar">
     <b-col
       cols="12"
       sm="6"
@@ -20,20 +21,57 @@
       v-for="opc in opciones"
       :key="opc.nombre"
     >
-      <router-link to="/comics">
-        <h1>{{ opc.nombre }}</h1>
+      <router-link :to="opc.url" style="text-decoration: none; color: inherit">
+        <!-- Para comics-->
+        <b-carousel
+          v-if="opc.nombre == 'Comics'"
+          slide
+          ref="Comics"
+          img-height="350"
+          :interval="3000"
+        >
+          <b-carousel-slide
+            v-for="img in imgComics"
+            :key="img.index"
+            :img-src="img.img"
+            :class="img.class"
+          >
+            <b-row class="etiquetaOpc">
+              <h1>{{ opc.nombre }}</h1>
+            </b-row>
+          </b-carousel-slide>
+        </b-carousel>
+        <!-- Para Personajes-->
+        <b-carousel
+          v-if="opc.nombre == 'Personajes'"
+          ref="Personajes"
+          img-width="200"
+          img-height="350"
+          :interval="3000"
+        >
+          <b-carousel-slide
+            v-for="img in imgPersonajes"
+            :key="img.index"
+            :img-src="img.img"
+            :class="img.class"
+          >
+            <b-row class="etiquetaOpc">
+              <h3>{{ opc.nombre }}</h3>
+            </b-row>
+          </b-carousel-slide>
+        </b-carousel>
       </router-link>
-      <b-img
-        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-        fluid
-        v-bind="imgOpcProps"
-        class="centerImg"
-      ></b-img>
+    </b-col>
+
+    <b-col cols="12" sm="6" md="4" class="columnaOpcion">
+      <h1>PlaceHolder</h1>
     </b-col>
   </b-row>
 </template>
 
 <script>
+import { publicKey } from "../marvel";
+import axios from "axios";
 export default {
   name: "PaginaInicio",
   data() {
@@ -41,37 +79,90 @@ export default {
       opciones: [
         {
           nombre: "Comics",
+          url: "/comics",
         },
         {
           nombre: "Personajes",
-        },
-        {
-          nombre: "Placeholder",
+          url: "/personajes",
         },
       ],
       imgOpcProps: {
         blankColor: "#bbb",
-        width: 450,
-        height: 55,
+        width: 300,
       },
       logoProps: {
         blankColor: "#bbb",
         width: 350,
         height: 450,
       },
+      imgComics: [],
+      imgPersonajes: [],
     };
   },
   setup() {
     const urlLogo = require("@/assets/MarvelComics.png");
     return { urlLogo };
   },
+  mounted() {
+    this.getImgComics();
+    this.getImgPersonajes();
+  },
   methods: {
-    url(opc) {
-      if (opc == "Comics") {
-        return "/comics";
-      } else if (opc == "Personajes") {
-        return "/"; //Cambiar
-      } else return "/"; //cambiar
+    getImgComics() {
+      axios
+        .get(
+          `http://gateway.marvel.com/v1/public/comics?orderBy=title&limit=10&apikey=${publicKey}`
+        )
+        .then((result) => {
+          var i = 0;
+          var tipo = "active";
+          result.data.data.results.forEach((item) => {
+            const comicimg = {
+              img: item.thumbnail.path + "." + item.thumbnail.extension,
+              class: tipo,
+              index: i,
+            };
+            this.imgComics.push(comicimg);
+            if (tipo == "active") {
+              tipo = "";
+            }
+            i++;
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    getImgPersonajes() {
+      axios
+        .get(
+          `http://gateway.marvel.com/v1/public/characters?orderBy=name&limit=10&apikey=${publicKey}`
+        )
+        .then((result) => {
+          var i = 0;
+          var tipo = "active";
+          result.data.data.results.forEach((item) => {
+            const pimg = {
+              img: item.thumbnail.path + "." + item.thumbnail.extension,
+              class: tipo,
+              index: i,
+            };
+            this.imgPersonajes.push(pimg);
+            if (tipo == "active") {
+              tipo = "";
+            }
+            i++;
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    iniciar() {
+      this.$refs.Comics.start();
+      this.$refs.Personajes.start();
     },
   },
 };
@@ -80,6 +171,7 @@ export default {
 <style scoped>
 h1 {
   text-align: center;
+  color: white;
 }
 
 #opciones {
@@ -92,23 +184,14 @@ h1 {
   position: fixed;
   top: 0;
   left: 0;
-  background: url(../assets/Inicio.jpg) no-repeat center center;
+  background: url(../assets/fondo2.jpg) no-repeat center center;
   background-size: cover;
   background-color: black;
   transform: scale(1.1);
 }
 
 .columnaOpcion {
-  width: 300px;
-}
-
-.vertical-center {
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
+  width: 270px;
 }
 
 * {
@@ -116,6 +199,17 @@ h1 {
 }
 
 .centerImg {
+  display: block;
   margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 30px;
+}
+
+.mostrar {
+  position: relative;
+}
+
+.etiquetaOpc {
+  background: black;
 }
 </style>
